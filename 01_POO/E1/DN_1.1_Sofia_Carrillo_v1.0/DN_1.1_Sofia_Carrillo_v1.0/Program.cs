de44f8debb88f85ccd327b841d0854a34608fee6
Sofia Abigail Carrillo Ek <sofia.abigail.carrillo@gmail.com>
2023-04-14 17:37:27 -0500

    Ejercicios tema 1.

---
+++ 01_POO/E1/DN_1.1_Sofia_Carrillo_v1.0/DN_1.1_Sofia_Carrillo_v1.0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DN_1._1_Sofia_Carrillo_v1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vehicles sounds");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 01_POO/E2/DN_1.2_Sofia_Carrillo_v1.0/DN_1.1_Sofia_Carrillo_v1.0/DN_1.1_Sofia_Carrillo_v1.0/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+
+namespace DN_1._1_Sofia_Carrillo_v1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                String TypeCar = null;
+                Vehicles vehicle = null;
+
+                Console.WriteLine("Type the vehicle: ");
+                TypeCar = Console.ReadLine().ToLower();
+
+
+                switch (TypeCar)
+                {
+                    case "car":
+                        vehicle = new Car();
+                        break;
+
+                    case "truck":
+                        vehicle = new Truck();
+                        break;
+
+                    case "train":
+                        vehicle = new Train();
+                        break;
+
+                    default:
+                        Console.WriteLine("Vehicle not found");
+                        break;
+                }
+
+                if (vehicle != null)
+                {
+                    vehicle.VehiclesSound();
+
+                }
+
+            }
+        }
+    }
+}
