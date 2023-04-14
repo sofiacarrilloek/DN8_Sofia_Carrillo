0a1402d0c2cb47b3f210f62ceaea4217feab19cf
Sofia Abigail Carrillo Ek <sofia.abigail.carrillo@gmail.com>
2023-04-14 15:40:58 -0500

    Tema 1. Tutorial 2

---
+++ 01_POO/T2/OPP.ConsoleExample/AudioManager/Cat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace AudioManager
+{
+    public class Cat : Animal
+    {
+        public override void AnimalSound()
+        {
+            AudioFileReader audioFile = new AudioFileReader("Resource/cat.wav");
+            WaveOutEvent waveOutEvent = new WaveOutEvent();
+
+            waveOutEvent.Init(audioFile);
+            waveOutEvent.Play();
+
+            while (waveOutEvent.PlaybackState == PlaybackState.Playing)
+            {
+                System.Threading.Thread.Sleep(500);
+            }
+
+            waveOutEvent.Dispose();
+            audioFile.Dispose();
+
+        }
+        
+    }
+}
---
+++ 01_POO/T2/OPP.ConsoleExample/AudioManager/Cow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+
+
+namespace AudioManager
+{
+    public class Cow : Animal
+    {
+        public override void AnimalSound()
+        {
+            AudioFileReader audioFile = new AudioFileReader("Resource/cow.wav");
+            WaveOutEvent waveOutEvent = new WaveOutEvent();
+
+            waveOutEvent.Init(audioFile);
+            waveOutEvent.Play();
+
+            while (waveOutEvent.PlaybackState == PlaybackState.Playing)
+            {
+                System.Threading.Thread.Sleep(500);
+            }
+
+            waveOutEvent.Dispose();
+            audioFile.Dispose();
+
+        }
+    }
+}
