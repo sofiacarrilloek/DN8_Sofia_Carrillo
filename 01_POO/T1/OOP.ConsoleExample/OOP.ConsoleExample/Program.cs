528747c804970603e603e5e82853be26238e3f21
Sofia Abigail Carrillo Ek <sofia.abigail.carrillo@gmail.com>
2023-04-14 15:31:48 -0500

    Tema 1. Tutorial 1

---
+++ 01_POO/T1/OOP.ConsoleExample/OOP.ConsoleExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+
+namespace OOP.ConsoleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+
+            Animal animal = new Animal();
+            animal.AnimalSound();
+
+            Console.ReadKey();
+        }
+    }
+}
