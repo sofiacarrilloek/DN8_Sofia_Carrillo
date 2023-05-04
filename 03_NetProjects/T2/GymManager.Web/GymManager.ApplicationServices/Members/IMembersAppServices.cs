aab12e513979c6afa7f0c1b11f902dc4d36431e1
Sofia Abigail Carrillo Ek <sofia.abigail.carrillo@gmail.com>
2023-05-03 22:29:27 -0500

    Tema 3. Tutorial 2

---
+++ 03_NetProjects/T2/GymManager.Web/GymManager.ApplicationServices/Members/IMembersAppServices.cs
@@ -0,0 +1,14 @@
+﻿using GymManager.Core.Members;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManager.ApplicationServices.Members
+{
+    public interface IMembersAppServices
+    {
+        List<Member> GetMembers();
+    }
+}
