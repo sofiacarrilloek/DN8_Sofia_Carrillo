6858ad96a43cfe147e3e62f106f534a60e4f2335
Sofia Abigail Carrillo Ek <sofia.abigail.carrillo@gmail.com>
2023-05-06 17:51:29 -0500

    Tema 3. Tutorial 4

---
+++ 03_NetProjects/T4/GymManager.Web/GymManager.ApplicationServices/Members/MembersAppServices.cs
@@ -0,0 +1,54 @@
+﻿using GymManager.Core.Members;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManager.ApplicationServices.Members
+{
+    public class MembersAppServices : IMembersAppServices
+    {
+        private static List<Member> Members = new List<Member>();
+
+        public int AddMember(Member member)
+        {
+            member.Id = new Random().Next();
+
+            Members.Add(member);
+
+            return member.Id;
+        }
+
+        public void DeleteMember(int memberId)
+        {
+            var m = Members.Where(x => x.Id == memberId).FirstOrDefault();
+            Members.Remove(m);
+        }
+
+        public void EditMember(Member member)
+        {
+            var m = Members.Where(x => x.Id == member.Id).FirstOrDefault();
+            m.AllowNewsletter = member.AllowNewsletter;
+            m.BirthDay = member.BirthDay;
+            m.CityId = member.CityId;
+            m.Email = member.Email;
+            m.LastName = member.LastName;
+            m.Name = member.Name;
+
+        }
+
+        public Member GetMember(int memberId)
+        {
+            var m = Members.Where(x => x.Id == memberId).FirstOrDefault();
+            return m;
+        }
+
+        public List<Member> GetMembers()
+        {
+
+            return Members;
+        }
+
+    }
+}
