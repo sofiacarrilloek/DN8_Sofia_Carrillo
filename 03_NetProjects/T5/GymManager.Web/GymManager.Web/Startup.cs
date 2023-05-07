76899130a2ce63f459fefefef9f15596cceaa5c0
Sofia Abigail Carrillo Ek <sofia.abigail.carrillo@gmail.com>
2023-05-06 19:55:12 -0500

    Tutorial 5

---
+++ 03_NetProjects/T5/GymManager.Web/GymManager.Web/Startup.cs
@@ -0,0 +1,55 @@
+//tutorial 5
+using GymManager.ApplicationServices.Members;
+using GymManager.ApplicationServices.Navigation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GymManager.Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+            services.AddTransient<IMembersAppServices, MembersAppServices>();
+
+            services.AddTransient<IMenuAppService, MenuAppService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=AttendanceController}/{action=MemberOut}/{id?}");
+
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=AttendanceController}/{action=MemberIn}/{id?}");
+
+            });
+        }
+    }
+}
