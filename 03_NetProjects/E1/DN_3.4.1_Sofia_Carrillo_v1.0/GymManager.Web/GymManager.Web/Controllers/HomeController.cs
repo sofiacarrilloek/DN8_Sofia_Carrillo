a01fb481ba12bbbf7b17ba795bf129b92f1c7444
Sofia Abigail Carrillo Ek <sofia.abigail.carrillo@gmail.com>
2023-06-20 11:39:09 -0500

    Tema 3. Ejercicio 1

---
+++ 03_NetProjects/E1/DN_3.4.1_Sofia_Carrillo_v1.0/GymManager.Web/GymManager.Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using GymManager.ApplicationServices.Members;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMembersAppServices _membersAppServices;
+
+        public HomeController(IMembersAppServices membersAppServices)
+        {
+            _membersAppServices = membersAppServices;
+        }
+        public IActionResult Index()
+        {
+            var members = _membersAppServices.GetMembers(); 
+
+            return View(members);
+        }
+    }
+
+
+}
