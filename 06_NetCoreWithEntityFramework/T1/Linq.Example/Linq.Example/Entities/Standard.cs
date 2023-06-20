893060c80878bb6fb34b861edf7cb33daa68c6ad
Sofia Abigail Carrillo Ek <sofia.abigail.carrillo@gmail.com>
2023-06-20 11:34:38 -0500

    Tema 6. Tutorial1

---
+++ 06_NetCoreWithEntityFramework/T1/Linq.Example/Linq.Example/Entities/Standard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq.Example.Entities
+{
+    class Standard
+    {
+        public int StandardID { get; set; }
+
+        public String StandardName { get; set; }
+    }
+}
---
+++ 06_NetCoreWithEntityFramework/T1/Linq.Example/Linq.Example/Program.cs
@@ -0,0 +1,63 @@
+﻿using Linq.Example.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq.Example
+{
+    class Program
+    {
+        static IList<Student> studentList = new List<Student>(){
+            new Student() { StudentID = 1, StudentName="John", Age=18, StandardID=1},
+            new Student() {StudentID = 2, StudentName="Steve", Age=21, StandardID=1},
+            new Student() {StudentID = 3, StudentName="Bill", Age=18, StandardID=2},
+            new Student() {StudentID = 4, StudentName="Ram", Age=20, StandardID=2},
+            new Student() {StudentID = 5, StudentName="Ron", Age=21 },
+
+                new Student() {StudentID = 6, StudentName="Morty", Age=14
+            }
+        };
+
+        static IList<Standard> standardList = new List<Standard>(){
+            new Standard() {StandardID = 1, StandardName="Standard 1"},
+            new Standard() {StandardID = 2, StandardName="Standard 2"},
+            new Standard() {StandardID = 3, StandardName="Standard 3"}
+        };
+
+
+        static void Main(string[] args)
+        {
+            var test = studentList.Where(x => (x.Age >= 18 && x.StandardID > 0))
+                .Select(x => x.StudentName);
+
+            var teenStudentsName = from s in studentList
+                                   where s.Age > 12 && s.Age < 20
+                                   select new { Name2 = s.StudentName };
+
+            var studentsGroupByStandard = from s in studentList
+                                          group s by s.StandardID into sg
+                                          orderby sg.Key
+                                          select new
+                                          {
+                                              StandardID = sg.Key,
+                                              Items = sg
+                                          };
+
+            var studentWithStandard = from s in studentList
+                                      join stad in standardList
+                                      on s.StandardID equals stad.StandardID
+                                      select new
+                                      {
+                                          StudentName = s.StudentName,
+                                          StandardName = stad.StandardName
+                                      };
+
+            foreach (var element in studentWithStandard)
+            {
+                Console.WriteLine($"Student: {element.StudentName} is in {element.StandardName}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
