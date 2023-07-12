2cae74ca5d3efd5934bdf8cb8cd2fb4527a9c535
Sofia Abigail Carrillo Ek <sofia.abigail.carrillo@gmail.com>
2023-07-12 12:43:38 -0500

    Tema 3. Tutorial 7

---
+++ 03_NetProjects/T7/GymManager.Web/GymManager.Web/Controllers/MembersController.cs
@@ -0,0 +1,75 @@
+﻿using GymManager.ApplicationServices.Members;
+using GymManager.Core.Members;
+using GymManager.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymManager.Web.Controllers
+{
+    [Authorize]
+    public class MembersController : Controller
+    {
+        private readonly IMembersAppServices _membersAppServices;
+        public MembersController(IMembersAppServices membersAppServices)
+        {
+            _membersAppServices = membersAppServices;
+        }
+
+        public IActionResult Index()
+        {
+            List<Member> members = _membersAppServices.GetMembers();
+
+            MemberListViewModel viewModel = new MemberListViewModel();
+
+            viewModel.NewMembersCount = 2;
+            viewModel.Members = members;
+
+
+            return View(viewModel);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Delete(int memberId)
+        {
+            _membersAppServices.DeleteMember(memberId);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Member member)
+        {
+            _membersAppServices.AddMember(member);
+
+            return RedirectToAction("Index");
+        }
+        
+        public IActionResult Edit(int memberId)
+        {
+            Member member = _membersAppServices.GetMember(memberId);
+
+            return View(member);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Member member)
+        {
+            _membersAppServices.EditMember(member);
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ 03_NetProjects/T7/GymManager.Web/GymManager.Web/Models/MenuViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GymManager.Core.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymManager.Web.Models
+{
+    public class MenuViewModel
+    {
+        public string CurrentPageName { get; set; }
+
+        public List<UserMenuItem> Menu { get; set; }
+    }
+}
