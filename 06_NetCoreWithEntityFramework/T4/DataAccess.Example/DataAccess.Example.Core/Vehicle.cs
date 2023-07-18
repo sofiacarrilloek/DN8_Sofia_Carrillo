56469b37724f8ee99e6982e6973675a698a3507b
Sofia Abigail Carrillo Ek <sofia.abigail.carrillo@gmail.com>
2023-07-18 12:27:13 -0500

    Tutorial 4. T6

---
+++ 06_NetCoreWithEntityFramework/T4/DataAccess.Example/DataAccess.Example.Core/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Example.Core
+{
+    public class Vehicle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public Make Make { get; set; }
+
+        public Model Model { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+    }
+}
