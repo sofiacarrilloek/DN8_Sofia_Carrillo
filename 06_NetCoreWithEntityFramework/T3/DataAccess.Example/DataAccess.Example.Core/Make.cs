1d22d1e38da4814eb52a7784d0bb33771501c857
Sofia Abigail Carrillo Ek <sofia.abigail.carrillo@gmail.com>
2023-07-18 09:29:10 -0500

    Tema 6. Tutorial 3

---
+++ 06_NetCoreWithEntityFramework/T3/DataAccess.Example/DataAccess.Example.Core/Make.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Example.Core
+{
+    public class Make
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string MakeName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public int Country { get; set; }
+
+       
+    }
+}
